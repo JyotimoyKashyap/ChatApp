b7039fc97eaa246ff9f2b788cd685b9009a91e44
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-12-25 10:12:57 +0530

    Added splash screen logic

--- app/src/main/java/io/github/jyotimoykashyap/chatapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/io/github/jyotimoykashyap/chatapp/ui/splash/SplashFragment.kt
@@ -1,11 +1,21 @@
 package io.github.jyotimoykashyap.chatapp.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import io.github.jyotimoykashyap.chatapp.R
+import io.github.jyotimoykashyap.chatapp.databinding.FragmentSplashBinding
+import io.github.jyotimoykashyap.chatapp.repository.BranchApiRepository
+import io.github.jyotimoykashyap.chatapp.ui.util.Resource
+import io.github.jyotimoykashyap.chatapp.viewmodels.SplashViewModel
 
 
 private const val ARG_PARAM1 = "param1"
@@ -16,6 +26,17 @@ class SplashFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    private val splashViewModel: SplashViewModel by viewModels{
+        val repository = BranchApiRepository()
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SplashViewModel(branchApiRepository = repository) as T
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +49,49 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeChanges()
+        initUi()
+    }
+
+    private fun initUi() {
+        // api call for getting messages
+        splashViewModel.getAllMessages()
+    }
+
+    private fun observeChanges() {
+        splashViewModel.messages.observe(viewLifecycleOwner){
+            when(it) {
+                is Resource.Success -> {
+                    // move to home page
+                    Handler(Looper.getMainLooper()).postDelayed(Runnable {
+                        findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+                    }, 2000)
+                }
+                is Resource.Loading -> {
+                    // stay on the splash screen
+                }
+                is Resource.Error -> {
+                    // move to login screen
+                    Handler(Looper.getMainLooper()).postDelayed(Runnable {
+                        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+                    }, 2000)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
---
+++ app/src/main/java/io/github/jyotimoykashyap/chatapp/viewmodels/SplashViewModel.kt
@@ -0,0 +1,36 @@
+package io.github.jyotimoykashyap.chatapp.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.jyotimoykashyap.chatapp.models.postmessage.MessageResponse
+import io.github.jyotimoykashyap.chatapp.repository.BranchApiRepository
+import io.github.jyotimoykashyap.chatapp.ui.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class SplashViewModel(
+    private val branchApiRepository: BranchApiRepository
+): ViewModel() {
+
+    // get all messages api call
+    private val _messages = MutableLiveData<Resource<List<MessageResponse>>>()
+    var messages = _messages
+
+    fun getAllMessages() = viewModelScope.launch {
+        _messages.postValue(Resource.Loading())
+
+        val response = branchApiRepository.getAllMessages()
+        _messages.postValue(handleResponse(response))
+    }
+
+    private fun handleResponse(response: Response<List<MessageResponse>>): Resource<List<MessageResponse>> {
+        if(response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+}
